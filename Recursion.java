4c78d69a2f37acf359ce3742bb7150029d19a50f
liulonghaohao <45375735+liulonghaohao@users.noreply.github.com>
2018-12-27 14:53:49 +0800

    两个递归方式，暂时保存

--- Recursion.java
+++ Recursion.java
@@ -4,6 +4,12 @@ public class Recursion{
    
    
    //part2: factorial recursion
+    public static int factorial(int n) {
+    	if(n==1||n==0) {
+    		return 1;
+    	}
+    	return n*factorial(n-1);
+    }
    
    
    
@@ -12,6 +18,12 @@ public class Recursion{
    
    
    //part4: fibonacci sequence recursion
+   public static int feedrabbit(int n) {
+    	if(n==1||n==2) {
+    		return 1;
+    	}
+    	return feedrabbit(n-1)+feedrabbit(n-2);
+    }
 
 
 }
